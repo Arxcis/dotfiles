b60a19d02a1d2faf580131a8d515d9bb96110a0f
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2017-09-27 11:05:05 +0200

    Latest changes

--- .profile
+++ .profile
@@ -1,9 +1,16 @@
 # Created: 13.08.17
 
-export PATH=$PATH:/Users/jsolsvik/bin
+export PATH=$PATH:/Users/jsolsvik/bin:~/go/bin
+export GOPATH=~/go
+export IPFS_PATH=~/.ipfs
 
 alias ll="ls -al"
 
 function allin {
 	git add . && git commit -m "$1"
 }
+
+function brewcask-upgrade {
+  brew cask uninstall $1 && brew cask install $1
+}
+
--- install.sh
+++ install.sh
@@ -45,6 +45,7 @@ brew install cloc    # Line counter
 brew install ipfs    # Interplanetary file system
 brew install heroku  # cloud PaaS
 brew install mysql   # mysql database shell
+brew install nmap
 
 #
 # brew cask - https://caskroom.github.io
@@ -54,6 +55,14 @@ brew tap caskroom/cask
 # Devenvs
 brew cask install dotnet
 brew cask install google-cloud-sdk
+brew cask install docker
+
+# Tools
+brew cask install postman
+brew cask install transmission
+brew cask install ethereum wallet
+brew cask install wireshark
+brew cask install virtualbox
 
 # Browsers
 brew cask install google-chrome
@@ -64,11 +73,13 @@ brew cask install sublime-text
 brew cask install atom
 brew cask install visual-studio-code
 
-# Media
+# Entertainment
 brew cask install vlc
 brew cask install spotify
+brew cask install minecraft
 
 # Communication
+brew cask install slack
 brew cask install caprine
 brew cask install discord
 brew cask install whatsapp
